24c2bb139ac3ddf56ddf2c7b36e72e51214d0c43
Danyal Ahmed <danyalskillorbit@users.noreply.github.com>
2019-02-22 11:56:58 +0500

    log

---
+++ BurmaTaskForce/Btf.Data.Model/LogRequest/LogMsg.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Btf.Data.Model.Base;
+
+namespace Btf.Data.Model.Log
+{
+    public class LogMsg : BaseEntity
+    {
+        public int LogRequestId { get; set; }
+        public LogRequest LogRequest { get; set; }
+        public string LogMsgType { get; set; }
+        public string Msg { get; set; }
+        public string StackTrace { get; set; }
+    }
+}
